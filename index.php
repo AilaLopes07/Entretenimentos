76d8ba7457b7a95e30ac49f9fd7af4e4aaaa0127
Teuteteu1 <165073413+Teuteteu1@users.noreply.github.com>
2024-07-03 15:23:49 -0300

    Informações sendo apresentadas
    Coloquei as informações que eram para aparecer e também fiz o card dos recentes embora tenha algumas informações no banco de dados que não estejam preenchidas ou o banco tenha que ser alterado tipo a categoria que não tem ligação direta com listagem_entretenimentos.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . "/auxiliar.php";
 
-$sql = "SELECT le.nome, le.faixa_etaria, le.diretor, le.duracao, le.sinopse, ce.nome as categoria FROM listagem_entretenimentos as le INNER JOIN listagem_entretenimentos_connect_categorias as lec on le.id = lec.id_listagem_entretenimentos LEFT JOIN categorias_entretenimento as ce on ce.id = lec.id_categorias LIKE '%estrelas%' ;";
+$sql = "SELECT le.nome, le.faixa_etaria, le.diretor, le.duracao, le.sinopse, le.id_tipo, ce.nome as categoria FROM listagem_entretenimentos as le INNER JOIN listagem_entretenimentos_connect_categorias as lec on le.id = lec.id_listagem_entretenimentos LEFT JOIN categorias_entretenimento as ce on ce.id = lec.id_categorias LIKE '%estrelas%' ;";
 
 $query = mysqli_query($conn, $sql);
 
@@ -9,6 +9,24 @@ $result = mysqli_fetch_assoc($query);
 
 print_r($result);
 
+$nome_filme = $result['nome'];
+
+
+$id_tpmain = $result['id_tipo'];
+
+
+$tipo_query = "  SELECT
+    `listagem_entretenimentos`.id_tipo, `tipos_entretenimento`.nome
+    FROM `listagem_entretenimentos`
+    INNER JOIN `tipos_entretenimento`
+    ON `listagem_entretenimentos`.id_tipo = `tipos_entretenimento`.id
+    WHERE `listagem_entretenimentos`.id = $id_tpmain";
+
+    $tipo_pesquisa = mysqli_query($conn, $tipo_query);
+
+    $resultado_pesquisa = mysqli_fetch_assoc($tipo_pesquisa);
+
+    $tipo_main = $resultado_pesquisa['nome'];
 /// $result['informações'] --- nome, faixa_etaria, diretor, duracao, sinopse, categoria;
 
 ?>
@@ -19,7 +37,7 @@ print_r($result);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Filme - <?= $result['nome'] ?></title>
+    <title><?= $tipo_main ?> - <?= $result['nome'] ?></title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -41,10 +59,9 @@ print_r($result);
         <nav>
             <ul>
                 <h3>Siga as nossas redes sociais</h3>
-                <li>Instagram</li>
-                <li>Twitter</li>
-                <li>Facebook</li>
-
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Facebook</a></li>
             </ul>
         </nav>
 
@@ -52,61 +69,108 @@ print_r($result);
             <h2>Sobre o(a) tipo</h2>
 
             <div>
-                <strong>Nome:</strong>
+                <strong>Nome: <?= $result['nome'] ?></strong>
             </div>
 
             <div>
-                <strong>Faixa Etária:</strong>
+                <strong>Faixa Etária: <?= $result['faixa_etaria']?></strong>
             </div>
 
             <div>
-                <strong>Duração:</strong>
+                <strong>Duração: <?= $result['duracao']?></strong>
             </div>
 
             <div>
-                <strong>Diretor:</strong>
+                <strong>Diretor: <?= $result['diretor'] ?></strong>
             </div>
 
             <div>
-                <strong>Lançamento:</strong>
+                <strong>Lançamento: </strong> <!-- Não tem informação, eu acho -->
             </div>
 
             <div>
-                <strong>Categorias:</strong>
+                <strong>Categorias: <?= $result['categoria']?></strong> <!-- Não tem também , vai ter que mudar no banco de dados depois -->
             </div>
         </main>
 
-        <div class="propraganda">
+        <div class="propaganda">
             <p>ESPAÇO PARA A PROPAGANDA</p>
         </div>
 
         <main>
             <h2>Sinopse</h2>
-            <p>sinopse</p>
+            <p><?= $result['sinopse'] ?></p>
         </main>
 
         <main>
             <h2>Recentes</h2>
+            <div class="grid">
+
+
+            <?php
+            $query_recentes = "SELECT * FROM listagem_entretenimentos WHERE nome <> '$nome_filme' limit 6;";
+
+            $recentes = mysqli_query($conn, $query_recentes);
+
+            $recentes_result = mysqli_fetch_all($recentes, MYSQLI_ASSOC);
+
+            // var_dump($recentes_result);
+
+
+
+            foreach ($recentes_result as $rec => $value) {
+                $r_nome = $value['nome'];
+                $r_diretor  = $value['diretor'];
+                $r_dur = $value['duracao'];
+                $r_class = $value['faixa_etaria'];
+                $id_tp = $value['id_tipo'];
+
+
+                $tipo_query = "  SELECT
+                    `listagem_entretenimentos`.id_tipo, `tipos_entretenimento`.nome
+                    FROM `listagem_entretenimentos`
+                    INNER JOIN `tipos_entretenimento`
+                    ON `listagem_entretenimentos`.id_tipo = `tipos_entretenimento`.id
+                    WHERE `listagem_entretenimentos`.id = $id_tp";
+
+                    $tipo_pesquisa = mysqli_query($conn, $tipo_query);
+
+                    $resultado_pesquisa = mysqli_fetch_assoc($tipo_pesquisa);
+
+
+
+        
+
+            
+            ?>
 
             <section class="recentes">
                 <header>
                     <div>
-                        <span>Filme</span>
+                        <span><?= $r_nome ?></span>
                     </div>
                 </header>
                 <hr>
                 <main>
                     <div>
-                        <li>Diretor:</li>
-                        <li>Duração:</li>
-                        <li>Classificação: </li>
+                        <li>Diretor: <?= $r_diretor ?></li>
+                        <li>Duração: <?= $r_dur ?></li>
+                        <li>Classificação: <?= $r_class ?></li>
                     </div>
                 </main>
                 <footer>
-                    <span>Link</span>
+                    <a href="#"><span>Link <?= $resultado_pesquisa['nome'] ?></span></a>
                 </footer>
             </section>
+
+            <?php
+            }
+            ?>
+
+
+
         </main>
+        </div>
         <a href="#">Topo</a>
     </section>
 
