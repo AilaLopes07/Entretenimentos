8785bd42f15c9484b229f217344cba31f98f959b
jandersonso <jandersonsoares15@gmail.com>
2024-07-07 10:45:03 -0300

    correcao no titulo da pagina estava dando warning

--- pesquisa.php
+++ pesquisa.php
@@ -24,7 +24,7 @@ $result = mysqli_fetch_assoc($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $tipo_main ?> - <?= $result['nome'] ?></title>
+    <title><?= $result['nome'] ?></title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
