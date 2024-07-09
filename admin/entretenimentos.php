ffb64727bef17c146f83cdf41c04682e5756bfc4
jandersonso <jandersonsoares15@gmail.com>
2024-07-09 19:54:54 -0300

    impressao dos entretenimentos

--- admin/entretenimentos.php
+++ admin/entretenimentos.php
@@ -22,6 +22,22 @@ move_uploaded_file ($_FILES["imagem"]["tmp_name"],$imagem);
 // $SQLIMAGEM = "INSERT INTO listagem_entretenimentos (imagem) VALUES ('$imagem') WHERE id = 1";
 // $result = mysqli_query($conn, $SQLIMAGEM);
 
+$query = "SELECT * FROM listagem_entretenimentos";
+
+$select = mysqli_query($conn, $query);
+
+$table = mysqli_fetch_all($select, MYSQLI_ASSOC);
+
+
+// $query = "SELECT le.id, le.nome, le.imagem, te.nome as tipo, le.faixa_etaria, le.diretor, le.sinopse, le.duracao, le.lancamento, ce.nome as categoria
+// FROM listagem_entretenimentos as le
+// INNER JOIN tipos_entretenimento as te on le.id_tipo = te.id
+// INNER JOIN listagem_entretenimentos_connect_categorias as lec on le.id = lec.id_listagem_entretenimentos
+// LEFT JOIN categorias_entretenimento as ce on ce.id = lec.id_categorias WHERE le.nome ";
+
+// $select = mysqli_query($conn, $query);
+// $table = mysqli_fetch_assoc($select);
+
 
 
 ?>
@@ -32,6 +48,39 @@ move_uploaded_file ($_FILES["imagem"]["tmp_name"],$imagem);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+
+body{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+table{
+    border-collapse: collapse;
+    width: 100%;
+}
+td{
+    border: 1px solid black;
+    padding: 5px;
+}
+
+thead{
+    text-align: center;
+    background-color: #b0db8c;
+}
+tbody tr:hover{
+    background-color: #f7f7f7;
+}
+.acoes{
+    text-align: center;
+}
+.apagar{
+    color: red;
+}
+.icon{
+    width: 20px;
+}
+
+
+</style>
 </head>
 <body>
 
@@ -41,6 +90,39 @@ move_uploaded_file ($_FILES["imagem"]["tmp_name"],$imagem);
     <input type= "file" name = "imagem" accept = "image/*"></p>
     <button type="submit"> enviar arquivo </button>
 </form>
+
+<table>
+        <thead>
+            <td>ID</td>
+            <td>Nome</td>
+            <td>faixa_etaria</td>
+            <td>duracao</td>
+            <td>diretor</td>
+            <td>lancamento</td>
+            <td>sinopse</td>
+            
+        </thead>
+        <tbody>
+            <?php
+            foreach ($table as $tabela => $value) {
+                
+                echo '<tr>'.
+                    '<td>'.$value['id'].'</td>'.
+                    '<td>' .$value['nome'].'</td>'.
+                    '<td>' .$value['faixa_etaria'].'</td>'.
+                    '<td>' .$value['duracao'].'</td>'.
+                    '<td>' .$value['diretor'].'</td>'.
+                    '<td>' .$value['lancamento'].'</td>'.
+                    '<td>' .$value['sinopse'].'</td>'.
+
+                    
+
+
+                    '<td class="acoes"><a href="editar.php?id='.$value['id'].'">editar</a>  <a class="apagar" href="apagar.php?id='.$value['id'].'">apagar</a></td>'.
+                    '</tr>';
+            }
+            
+            ?>
 
 </body>
 </html>
